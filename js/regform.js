5d2232e985bc533fb8daa75fda22472f8c6704ec
unknown <akshatha1203@gmail.com>
2018-07-19 16:14:23 +0530

    initial commit

--- js/regform.js
+++ js/regform.js
@@ -1,37 +1,42 @@
 $(function(){
- $("#regform").validate({
+    $(".submit").click(function(){
+var isValid= $("#regform").validate({
      rules:{
          usn:{
              required:true,
-             minlength:10,
-             maxlength:10,
+            
          },
          name:{
               required:true,
-               
+              minlength:2,
+              maxlength:50,
      },
          email:{
              required:true,
-         
+             email:true,
      },
      mobile:{
             required:true,
-         
+            minlength:10,
+             maxlength:10,
       },
-        course:{
-               required:true,
+         coures:{
+             required:true,
+         },
+        
                         
-               },
         percentage:{ 
                   required:true,
-                      
-        },
+                  min:55,
+                max:100,
+        }
          
      },
     
         messages:{
                 usn:{
                      required:"USN can't be empty"
+                    
                         },
                 name:{
                      required:"Name can't be empty"
@@ -44,12 +49,38 @@ $(function(){
                        required:"mobile no. can't be empty"
                         
                         }, 
-                courses:{
-                        
-                        },
+             coure:{ 
+                  required:"course can't be empty"
+                  },
+               
                 percentage:{
-                    
-                },        
+                    required:"percentage can't be empty",
+                    min :"below 55 can not eligible",
+                    max:"eligible"
+                }        
                         }
- });
+ }).form();
+        if(isValid){
+            var usn = $("#usn").val();
+            var name = $("#name").val();
+            var email = $("#email").val();
+            var mobile = $("#mobile").val();
+            var course = $("#course").val();
+            var percentage = $("#percentage").val();
+                             $(".reset").click();
+            
+            student = {
+                "usn":usn,
+                "name":name,
+                "email":email,
+                "mobile":mobile,
+                "course":course,
+                "percentage":percentage
+            }
+            
+            console.log(student);
+            
+            return false;
+        }
+    });
 });
